3012ea0d8e3a61130d94fd4e9e380f96cb25a036
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-03-09 23:53:13 -0300

    ADD: Site title.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      title: "Stable Diffusion generator", 
+    }
+  },
   modules: [
     "@nuxt/ui"
   ],
   runtimeConfig: {
     stableDiffusionKey: process.env.STABLE_DIFFUSION_KEY
-  }
+  },
+  colorMode: {
+    preference: "dark"
+  },
 })
