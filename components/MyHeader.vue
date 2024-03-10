16609848f67da38ad58f1bbe9ec62c241be28dcf
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-03-09 23:56:06 -0300

    ADD: Header.

---
+++ components/MyHeader.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <header class="p-3 bg-cloud-burst-900 border-b border-b-cloud-burst-800">
+    <UContainer class="flex justify-center md:justify-between items-center">
+      <nuxt-link to="/">
+        <img src="/logo.svg" alt="Site logo"/>
+      </nuxt-link>
+    </UContainer>
+  </header> 
+</template>
+
