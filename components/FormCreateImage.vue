8a015840a3d888c79b58d20813b594039b410f1c
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-03-10 21:55:42 -0300

    ADD:  Popover with description of each form field.

--- components/FormCreateImage.vue
+++ components/FormCreateImage.vue
@@ -106,33 +106,170 @@
     <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
       <h1 class="font-bold text-lg">Create a new image</h1>
       <!-- Prompt -->
-      <UFormGroup label="Prompt" name="prompt">
-        <UTextarea v-model="state.prompt" placeholder="Insert prompt" />
+      <UFormGroup 
+        name="prompt"
+        :ui="{label: { base: 'flex justify-between items-center w-full mb-1'}}">
+        <template #label>
+          <span>Prompt</span>
+          <UPopover :ui="{wrapper: 'flex'}">
+            <UButton 
+              size="2xs" 
+              :padded="false" 
+              color="cloud-burst" 
+              :ui="{ rounded: 'rounded-full'}" 
+              icon="i-heroicons-information-circle-20-solid"
+            />
+            <template #panel>
+              <div class="p-4">
+                <p> The prompt is a parameter that tells Stable Diffusion <br>
+                  what you want to see in the generated images. <br>
+                  e.g. "a cat in the forest sleeping on a tree" <br>
+                </p>
+              </div>
+            </template>
+          </UPopover>
+        </template>
+        <template #default>
+          <UTextarea v-model="state.prompt" placeholder="Insert prompt" />
+        </template> 
       </UFormGroup>
 
       <!-- Negative Prompt -->
-      <UFormGroup label="Negative Prompt" name="negative_prompt">
-        <UTextarea v-model="state.negative_prompt" placeholder="Insert negative prompt" />
+      <UFormGroup 
+        name="negative_prompt"
+        :ui="{label: { base: 'flex justify-between items-center w-full mb-1'}}">
+        <template #label>
+          <span >Negative Prompt</span>
+          <UPopover :ui="{wrapper: 'flex'}">
+            <UButton 
+              size="2xs" 
+              :padded="false" 
+              color="cloud-burst" 
+              :ui="{ rounded: 'rounded-full'}" 
+              icon="i-heroicons-information-circle-20-solid"
+            />
+            <template #panel>
+              <div class="p-4">
+                <p> The negative prompt is a parameter that tells Stable Diffusion <br>
+                  what <strong>you don't want to see</strong>  in the generated images. <br>
+                  e.g. "blurry, low quality, not realistic"
+                </p>
+              </div>
+            </template>
+          </UPopover>
+        </template>
+        <template #default>
+          <UTextarea v-model="state.negative_prompt" placeholder="Insert negative prompt" />
+        </template> 
       </UFormGroup>
 
       <!-- Style Preset -->
-      <UFormGroup label="Style Preset" name="style_preset">
-        <USelect v-model="state.style_preset" :options="styles_preset" />
+      <UFormGroup 
+        name="style_preset"
+        :ui="{label: { base: 'flex justify-between items-center w-full mb-1'}}">
+        <template #label>
+          <span >Style Preset</span>
+          <UPopover :ui="{wrapper: 'flex'}">
+            <UButton 
+              size="2xs" 
+              :padded="false" 
+              color="cloud-burst" 
+              :ui="{ rounded: 'rounded-full'}" 
+              icon="i-heroicons-information-circle-20-solid"
+            />
+            <template #panel>
+              <div class="p-4">
+                <p> Style to guide the image model towards a particular style.</p>
+              </div>
+            </template>
+          </UPopover>
+        </template>
+        <template #default>
+          <USelect v-model="state.style_preset" :options="styles_preset" />
+        </template> 
       </UFormGroup>
 
       <!-- Seed -->
-      <UFormGroup label="Seed" name="seed">
-        <UInput v-model="state.seed" type="number" placeholder="Seed" />
+      <UFormGroup 
+        name="seed"
+        :ui="{label: { base: 'flex justify-between items-center w-full mb-1'}}">
+        <template #label>
+          <span >Seed</span>
+          <UPopover :ui="{wrapper: 'flex'}">
+            <UButton 
+              size="2xs" 
+              :padded="false" 
+              color="cloud-burst" 
+              :ui="{ rounded: 'rounded-full'}" icon="i-heroicons-information-circle-20-solid"
+            />
+            <template #panel>
+              <div class="p-4">
+                <p> A number for image generation. 
+                <strong>0 assigns a random seed.</strong>  
+                <br> a specific one reproduces identical results.</p>
+              </div>
+            </template>
+          </UPopover>
+        </template>
+        <template #default>
+          <UInput v-model="state.seed" type="number" placeholder="Seed" />
+        </template> 
       </UFormGroup>
 
       <!-- Steps -->
-      <UFormGroup label="Steps" name="steps" :hint="state.steps.toString()">
-        <URange :min="10" :max="20" v-model="state.steps" />
+      <UFormGroup 
+        name="steps" 
+        :ui="{label: { base: 'flex justify-between items-center w-full mb-1'}}">
+        <template #label>
+          <span >Steps: <strong>{{ state.steps.toString()}}</strong> </span>
+          <UPopover :ui="{wrapper: 'flex'}">
+            <UButton 
+              size="2xs" 
+              :padded="false" 
+              color="cloud-burst" 
+              :ui="{ rounded: 'rounded-full'}" 
+              icon="i-heroicons-information-circle-20-solid"
+            />
+            <template #panel>
+              <div class="p-4">
+                <p> Number of diffusion steps to run.</p>
+              </div>
+            </template>
+          </UPopover>
+        </template>
+        <template #default>
+          <URange :min="10" :max="20" v-model="state.steps" />
+        </template> 
       </UFormGroup>
 
       <!-- Cfg_scale -->
-      <UFormGroup label="CFG Scale" name="cfg_scale" :hint="state.cfg_scale.toString()">
-        <URange :min="0" :max="35" v-model="state.cfg_scale" />
+      <UFormGroup 
+        name="cfg_scale"
+        :ui="{label: { base: 'flex justify-between items-center w-full mb-1'}}">
+        <template #label>
+          <span >CFG Scale: <strong>{{ state.cfg_scale.toString()}}</strong> </span>
+          <UPopover :ui="{wrapper: 'flex'}">
+            <UButton 
+              size="2xs" 
+              :padded="false" 
+              color="cloud-burst" 
+              :ui="{ rounded: 'rounded-full' }" 
+              icon="i-heroicons-information-circle-20-solid"
+            />
+            <template #panel>
+              <div class="p-4">
+                <p>
+                  Adjusts how closely the image follows the text. <br>
+                  Higher values mean closer adherence to text but less diversity. <br> 
+                  Optimal between 7-12 for creative results.
+                </p>
+              </div>
+            </template>
+          </UPopover>
+        </template>
+        <template #default>
+          <URange :min="0" :max="35" v-model="state.cfg_scale" />
+        </template> 
       </UFormGroup>
 
       <!-- Submit -->
