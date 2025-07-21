f373bcf07a609e20d7356457b4c60fd002907a38
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-07-20 23:10:41 -0400

    refactor(imageGeneration): update seed handling in generated image data

--- stores/imageGeneration.ts
+++ stores/imageGeneration.ts
@@ -48,11 +48,11 @@ export const useImageGenerationStore = defineStore("imageGeneration",  {
   actions: {
     updateGeneratedImage(base64: string, seed: number, form: NewImageParamsUser) {
       this.generatedImage.isGenerated = true;
-      this.newImageParams.seed = seed;
       this.generatedImage.base64 = base64;
       this.generatedImage.data = {
         ...baseParams,
         ...form,
+        seed,
       };
     },
     updateLoadingState(isLoading: boolean) {
