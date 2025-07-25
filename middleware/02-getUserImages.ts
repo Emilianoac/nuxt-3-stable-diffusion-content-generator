21daabc329a1016d7756c77f1855b1f00167d7a0
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-07-25 13:59:48 -0400

    refactor(middleware): remove unused user images middleware

--- middleware/02-getUserImages.ts
+++
@@ -1,6 +0,0 @@
-export default defineNuxtRouteMiddleware( async (to) => {
-  // skip middleware on server
-  if (import.meta.server) return;
-  const store = useUserStore();
-  store.subscribeToUserImages();
-});
