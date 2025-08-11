a98e29bf9ca31be9b025ff8e884269b868d40386
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2025-08-11 09:56:04 -0400

    refactor(mocks): moving mocks into a dedicated folder

--- setupTests.ts
+++ setupTests.ts
@@ -1,8 +1,8 @@
 import { expect } from "vitest";
 import "@testing-library/jest-dom/vitest";
 import * as matchers from "@testing-library/jest-dom/matchers";
-import "@/__tests__/mocks/mockFirebaseClient";
-import "@/__tests__/mocks/plugins/mockAuthServicePlugin";
+import "@/__mocks__/firebase/mockFirebaseClient";
+import "@/__mocks__/plugins/mockAuthServicePlugin";
 
 expect.extend(matchers);
 
